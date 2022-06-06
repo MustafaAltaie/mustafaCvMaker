1ceabded25daa57e2187fac14655e4a7c87fe841
Mustafa <mustafaphoto111@gmail.com>
2022-06-06 23:53:42 +0200

    'final'

--- mutualScript.js
+++ mutualScript.js
@@ -9,7 +9,7 @@ function screenShot(){
 
 function db(){
     clientNameText.value = localStorage.clientName;
-    clientEmailText.value = localStorage.clientEmail;
+    clientEmailText.value = sessionStorage.clientEmail;
     clientNumberText.value = localStorage.clientNumber;
     clientTitleText.value = localStorage.clientTitle;
     clientAddressText.value = localStorage.clientAddress.replace('/', ' ');
@@ -28,7 +28,7 @@ function db(){
 if(document.querySelectorAll('#_IdDB').length != 0){
     _idText.value = _IdDB.value;
     localStorage.clientName = clientNameDB.value;
-    localStorage.clientEmail = clientEmailDB.value;
+    sessionStorage.clientEmail = clientEmailDB.value;
     localStorage.clientNumber = clientNumberDB.value;
     localStorage.clientTitle = clientTitleDB.value;
     localStorage.clientAddress = clientAddressDB.value.replace(' ', ' /');
@@ -101,34 +101,94 @@ nav.addEventListener('click', function(evt){
     window.location.href = '/';
 });
 
+
+
+
+// ----------------- Personal Image ----------------- //
+
+document.getElementById('changeImage').onclick = function(){
+    if(sessionStorage.clientEmail == undefined || sessionStorage.clientEmail == ''){
+        alert('Enter email address first!');
+        clientEmail.click();
+        editText.select();
+    }
+    else
+    imageFile.click()
+}
+
+
+
 document.addEventListener('click', function(evt){
     if(evt.target.parentNode.id != 'nav'){
         cvSizeRange.style.display = 'none';
         pSizeRange.style.display = 'none';
     }
 
-    if(evt.target.id == 'actionButton')
-    if(currentElement == 'clientEmail')
-    if(editText.value.includes('@')){
-        if(editText.value.includes('gmail')
-        || editText.value.includes('yahoo')
-        || editText.value.includes('hubSpo')
-        || editText.value.includes('sendinblue')
-        || editText.value.includes('aweber')
-        || editText.value.includes('protonMail')
-        || editText.value.includes('outlook')
-        || editText.value.includes('zoho')
-        || editText.value.includes('aol')
-        || editText.value.includes('gmx')
-        || editText.value.includes('icloud'))
-        document.getElementById(currentElement).innerHTML = editText.value;
+    if(evt.target.id == 'actionButton'){
+        if(currentElement == 'clientEmail')
+        if(editText.value.includes('@')){
+            if(editText.value.includes('gmail')
+            || editText.value.includes('yahoo')
+            || editText.value.includes('hubSpo')
+            || editText.value.includes('sendinblue')
+            || editText.value.includes('aweber')
+            || editText.value.includes('protonMail')
+            || editText.value.includes('outlook')
+            || editText.value.includes('zoho')
+            || editText.value.includes('aol')
+            || editText.value.includes('gmx')
+            || editText.value.includes('icloud'))
+            document.getElementById(currentElement).innerHTML = editText.value;
+            else{
+                alert('Email is not valid');
+                document.getElementById(currentElement).innerHTML = 'emailaddress@example.com';
+            }
+        }
         else{
             alert('Email is not valid');
-            document.getElementById(currentElement).innerHTML = 'example@gmail.com';
+            document.getElementById(currentElement).innerHTML = 'emailaddress@example.com';
         }
-    }
-    else{
-        alert('Email is not valid');
-        document.getElementById(currentElement).innerHTML = 'example@gmail.com';
+        setTimeout(function(){
+            if(clientEmail.innerHTML == 'emailaddress@example.com')
+            delete sessionStorage.clientEmail;
+        }, 10);
     }
 });
+
+
+var imageExt = '.jpg,.JPG,jpeg,JPEG,.bmp,.BMP,.gif,.GIF,.png,.PNG';
+
+var uploadPerm = 0;
+var rndNumber;
+
+document.getElementById('imageFile').onchange = function(){
+    if(imageFile.value != ''){
+        for(i = 0; i < imageExt.split(',').length; i++){
+            if(imageFile.value.substr(imageFile.value.length-4, imageFile.value.length) == imageExt.split(',')[i]){
+                uploadPerm = 1;
+                break;
+            }
+            else uploadPerm = 0;
+        }
+        if(uploadPerm == 0) alert('File is not image');
+        else{
+            if(changeImage.innerHTML == 'Add image'){
+                localStorage.imageName = clientEmail.innerHTML;
+                imageFileName.value = clientEmail.innerHTML;
+                imageUploadSubmit.click();
+            }
+            else{
+                imageFileName.value = sessionStorage.clientEmail;
+                imageUploadSubmit.click();
+            }
+        }
+    }
+}
+
+document.getElementById('removeImage').onclick = function(){
+    window.location.href = '/deleteImage/' + localStorage.imageName + '/' + cvName.value;
+    delete localStorage.imageName;
+}
+
+localStorage.clear();
+sessionStorage.clear();
