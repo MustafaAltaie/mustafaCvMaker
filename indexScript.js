fd8c8ac1551db2a049c7fe6811fc7ad29adb6f56
Mustafa <mustafaphoto111@gmail.com>
2022-06-09 23:00:47 +0200

    'design14'

--- indexScript.js
+++ indexScript.js
@@ -14,6 +14,16 @@ document.getElementById('sliderSpeedRange').onchange = function(){
     window.location.reload();
 }
 
+document.getElementById('sliderfold1Range').onchange = function(){
+    localStorage.sliderfold1Range = sliderfold1Range.value;
+    window.location.reload();
+}
+
+document.getElementById('sliderfold2Range').onchange = function(){
+    localStorage.sliderfold2Range = sliderfold2Range.value;
+    window.location.reload();
+}
+
 if(localStorage.sliderSpeedRange == undefined)
 var sliderSpeed = sliderSpeedRange.value;
 else{
@@ -21,32 +31,59 @@ else{
     var sliderSpeed = localStorage.sliderSpeedRange;
 }
 
-document.getElementById('page1').style.transform = 'perspective(1000px) rotateY(20deg)';
+if(localStorage.sliderfold1Range == undefined)
+var foldRight = sliderfold1Range.value;
+else{
+    sliderfold1Range.value = localStorage.sliderfold1Range;
+    var foldRight = localStorage.sliderfold1Range;
+}
+
+if(localStorage.sliderfold2Range == undefined)
+var foldLeft = sliderfold2Range.value;
+else{
+    sliderfold2Range.value = localStorage.sliderfold2Range;
+    var foldLeft = localStorage.sliderfold2Range;
+}
 
 for(i = 1; i <= demo2.querySelectorAll('div').length; i++){
     document.getElementById('page' + i).style.zIndex = demo2.querySelectorAll('div').length-i;
+    document.getElementById('page' + i).style.transform = 'perspective(1000px) rotateY(' + parseInt(foldLeft-i*1) + 'deg)';
     document.getElementById('page' + i).style.transition = sliderSpeed/1000 + 's';
 }
 
 var i = 1;
 var slideStage = 1;
+var stage1Play = 0;
+var stage2Play = 0;
 
 function slider(){
     if(slideStage == 1){
-        document.getElementById('page' + i).style.transform = 'perspective(1000px) rotateY(160deg)';
-        i++;
         setTimeout(function(){
-            document.getElementById('page' + parseInt(i-1)).style.zIndex = '';
-        }, sliderSpeed/2);
-        if(i > demo2.querySelectorAll('div').length)
-        slideStage = 2;
+            stage1Play = 1;
+        }, 2000);
+        if(stage1Play == 1){
+            document.getElementById('page' + i).style.transform = 'perspective(1000px) rotateY(' + parseInt(foldRight-i*1) + 'deg)';
+            i++;
+            setTimeout(function(){
+                document.getElementById('page' + parseInt(i-1)).style.zIndex = '';
+            }, sliderSpeed/2);
+            if(i > demo2.querySelectorAll('div').length)
+            slideStage = 2;
+            stage2Play = 0;
+        }
     }
     else{
-        i--;
-        document.getElementById('page' + i).style.transform = 'perspective(1000px) rotateY(20deg)';
-        document.getElementById('page' + i).style.zIndex = demo2.querySelectorAll('div').length-i;
-        if(i == 1)
-        slideStage = 1;
+        setTimeout(function(){
+            stage2Play = 1;
+        }, 2000);
+        if(stage2Play == 1){
+            i--;
+            document.getElementById('page' + i).style.transform = 'perspective(1000px) rotateY(' + parseInt(foldLeft-i*1) + 'deg)';
+            document.getElementById('page' + i).style.zIndex = demo2.querySelectorAll('div').length-i;
+            if(i == 1)
+            slideStage = 1;
+            stage1Play = 0;
+        }
     }
 }
 var sliderInterval = setInterval(slider, sliderSpeed);
@@ -63,7 +100,7 @@ document.addEventListener('visibilitychange', function(){
 
 
 
-setInterval(function(){
-    demo1.style.filter = 'hue-rotate(' + Math.ceil(Math.random()*360) + 'deg)';
-}, 1000);
+// setInterval(function(){
+//     demo1.style.filter = 'hue-rotate(' + Math.ceil(Math.random()*360) + 'deg)';
+// }, 1000);
 //delete localStorage.CVAuthorization;
